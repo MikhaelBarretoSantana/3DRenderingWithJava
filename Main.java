39d994e3ceb94d2b29c36c1ebdcab8e9056f3137
mikhaelBarretoSantana <mikhaelbarretos@gmail.com>
2024-03-27 16:27:04 -0300

    fix colors of tetraedro and implemented shades for improving the 3d experience

--- Main.java
+++ Main.java
@@ -8,6 +8,18 @@ import javax.swing.*;
 
 public class Main {
 
+    public static Color getShade(Color color, double shade) {
+    double redLinear = Math.pow(color.getRed(), 2.4) * shade;
+    double greenLinear = Math.pow(color.getGreen(), 2.4) * shade;
+    double blueLinear = Math.pow(color.getBlue(), 2.4) * shade;
+
+    int red = (int) Math.pow(redLinear, 1/2.4);
+    int green = (int) Math.pow(greenLinear, 1/2.4);
+    int blue = (int) Math.pow(blueLinear, 1/2.4);
+
+    return new Color(red, green, blue);
+}
+
     public static void main(String[] args) {
 
         JFrame frame = new JFrame();
@@ -70,13 +82,17 @@ public class Main {
 
                 BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
 
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+
+                for (int q = 0; q < zBuffer.length; q++) {
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
+
                 for (Triangle t : tris) {
                     Vertex v1 = transform.transform(t.v1);
                     Vertex v2 = transform.transform(t.v2);
                     Vertex v3 = transform.transform(t.v3);
 
-                    // since we are not using Graphics2D anymore,
-                    // we have to do translation manually
                     v1.x += getWidth() / 2;
                     v1.y += getHeight() / 2;
                     v2.x += getWidth() / 2;
@@ -91,7 +107,6 @@ public class Main {
                     int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
                     int maxY = (int) Math.min(img.getHeight() - 1,
                             Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
-                            
 
                     double triangleArea = (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
 
@@ -100,15 +115,34 @@ public class Main {
                             double b1 = ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
                             double b2 = ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
                             double b3 = ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                            Vertex ab = new Vertex(v2.x - v1.x, v2.y - v1.y, v2.z - v1.z);
+                            Vertex ac = new Vertex(v3.x - v1.x, v3.y - v1.y, v3.z - v1.z);
+                            
+                            Vertex norm = new Vertex(
+                                    ab.y * ac.z - ab.z * ac.y,
+                                    ab.z * ac.x - ab.x * ac.z,
+                                    ab.x * ac.y - ab.y * ac.x);
+
+                            double normalLength = Math.sqrt(norm.x * norm.x + norm.y * norm.y + norm.z * norm.z);
+                            norm.x /= normalLength;
+                            norm.y /= normalLength;
+                            norm.z /= normalLength;
+
+                            double angleCos = Math.abs(norm.z);
+
                             if (b1 >= 0 && b1 <= 1 && b2 >= 0 && b2 <= 1 && b3 >= 0 && b3 <= 1) {
-                                img.setRGB(x, y, t.color.getRGB());
+                                double depth = b1 * v1.z + b2 * v2.z + b3 * v3.z;
+                                int zIndex = y * img.getWidth() + x;
+                                if (zBuffer[zIndex] < depth) {
+                                    img.setRGB(x, y, getShade(t.color, angleCos).getRGB());
+                                    zBuffer[zIndex] = depth;
+                                }
                             }
                         }
                     }
 
                 }
                 g2.drawImage(img, 0, 0, null);
-
 
             }
 
